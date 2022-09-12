0ea6f2a0472f658afc56f9881ac14ebf75ed0ef7
frankieavina <frankieavina@gmail.com>
2022-09-12 16:02:25 -0700

    setting up project

---
+++ screens/AddFoodScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddFoodScreen = () => {
+  return (
+    <View>
+      <Text>AddFoodScreen</Text>
+    </View>
+  )
+}
+
+export default AddFoodScreen
+
+const styles = StyleSheet.create({})
---
+++ screens/DetailedLogScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DetailedLogScreen = () => {
+  return (
+    <View>
+      <Text>DetailedLogScreen</Text>
+    </View>
+  )
+}
+
+export default DetailedLogScreen
+
+const styles = StyleSheet.create({})
