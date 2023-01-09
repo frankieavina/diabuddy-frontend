b6db39d7ed3c9afa8406877a833ff4c754328fa2
frankieavina <frankieavina@gmail.com>
2023-01-09 15:59:34 -0800

    finished adding graph

--- common/api/nightScoutApi.js
+++ common/api/nightScoutApi.js
@@ -2,19 +2,24 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 export const nightScoutApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'frankiecgm.up.railway.app/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://frankiecgm.up.railway.app/' }),
   reducerPath: 'graphApi',
   endpoints: (build) => ({
 
     getUserGlucoseData: build.query({
-        query: ({date1, date2}) =>  `api/v1/entries/sgv.json?find[dateString][$gte]=${date1}&find[dateString][$lte]=${date2}`,
+        query: ({date1, date2}) =>  ({
+          url: `api/v1/entries/sgv.json?find[dateString][$gte]=${date1}&find[dateString][$lte]=${date2}&count=233`,
+          method: 'GET',
+        }),
         transformResponse: (response, meta, arg) => {
-            console.log('Response');
-            return {
-                xData: response,
-                yData: response
-            }
-        } 
+          return response.map((data) => {
+              console.log('jjjjjj:',data)
+              return {
+                date: data.date,
+                svg: data.sgv
+              }
+          })
+        }
     }),
 
   }),
--- features/administrator/features/UserGraph.js
+++ features/administrator/features/UserGraph.js
@@ -1,29 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Grid, LineChart, XAxis, YAxis, AreaChart } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
-import { Button } from '@rneui/themed';
+import { Button, Card } from '@rneui/themed';
 import { Colors } from '../../../utils/constants/colors';
 import { useGetUserGlucoseDataQuery } from '../../../common/api/nightScoutApi';
 import moment from 'moment';
 
 const UserGraph = ({onPress, date, user}) => {
 
-    const { item , id} = user;
-    
-    let formattedDate = moment(date).add(1, 'days');
-    
-    // console.log('Date:',moment(date).format("YYYY-MM-DD"));
-    // console.log('Date2:',moment(date2).format("YYYY-MM-DD"));
+    const [ dateData, setDateData ] = useState([]);
 
-    const {bData, isLoading} = useGetUserGlucoseDataQuery({
-        date1: moment(formattedDate).format("YYYY-MM-DD"),
-        date2: moment(formattedDate).add(1, 'days').format('YYYY-MM-DD')
+    const { item , id} = user;
+
+    const {data, isLoading} = useGetUserGlucoseDataQuery({
+        date1: moment(date).format("YYYY-MM-DD"),
+        date2: moment(date).add(1, 'days').format('YYYY-MM-DD')
     });
 
-    console.log("Bdata:",bData)
+    useEffect(()=>{
+        if(data){
+            data.map((data)=>{
+                const date1 = moment(date).format('YYYY-MM-DD');
+                const date2 = moment(data.date).format('YYYY-MM-DD');
+                const boolDate = moment(date1).isSame(date2);
+                if(boolDate){
+                    setDateData((prevState)=>[...prevState, data.svg]);
+                }
+            })
+        }
+    },[data])
 
-    const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
 
     const axesSvg = { fontSize: 10, fill: 'grey' };
     const verticalContentInset = { top: 10, bottom: 10 };
@@ -31,43 +38,46 @@ const UserGraph = ({onPress, date, user}) => {
 
   return (
     <View style={styles.charContainer}>
-        <View style={{ height: 350, padding: 20, flexDirection: 'row' }}>
-            <YAxis
-                data={data}
-                style={{ marginBottom: xAxisHeight }}
-                contentInset={verticalContentInset}
-                svg={axesSvg}
-            />
-            <View style={{ flex: 1, marginLeft: 10 }}>
-                <AreaChart
-                    style={{ height: 300 }}
-                    data={data}
-                    contentInset={{ verticalContentInset }}
-                    curve={shape.curveNatural}
-                    svg={{ fill: Colors.icon500}}
-                >
-                <Grid />
-                </AreaChart>
-                {/* <LineChart
-                    style={{ flex: 1 }}
-                    data={data}
-                    contentInset={verticalContentInset}
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
-                >
-                    <Grid/>
-                </LineChart> */}
-                <XAxis
-                    style={{ marginHorizontal: -10, height: xAxisHeight }}
-                    data={data}
-                    formatLabel={(value, index) => index}
-                    contentInset={{ left: 10, right: 10 }}
-                    svg={axesSvg}
-                />
+        { (dateData.length > 0)
+        ?
+            <View style={{ height: 350, padding: 20, flexDirection: 'row' }}>
+                {/* <View style={{ flex: 1, marginLeft: 10 }}> */}
+                    <YAxis
+                        data={dateData}
+                        contentInset={{top: 20, bottom: 20}}
+                        svg={{
+                            fill: 'grey',
+                            fontSize: 10,
+                        }}
+                        numberOfTicks={20}
+                        formatLabel={(value) => `${value} mg/dL`}
+                    />
+                    <LineChart
+                        style={{ flex: 1 }}
+                        data={dateData}
+                        contentInset={verticalContentInset}
+                        svg={{ stroke: 'rgb(134, 65, 244)' }}
+                        yMin={70}
+                        yMax={300}
+                    >
+                        <Grid/>
+                    </LineChart>
+                {/* </View> */}
+            </View> 
+        :
+            <View>
+                <Card containerStyle={{ marginTop: 15 }}>
+                    <Card.Title>No Data</Card.Title>
+                    <Card.Divider />
+                    <Text style={styles.fonts} h3>
+                        No data found for this date. Please go back and select a different date.
+                    </Text>
+                </Card>
             </View>
-        </View>
+        }
         <Button buttonStyle={{backgroundColor:Colors.primary500, width:'50%', margin:20}} onPress={onPress}>
                 Go Back
-        </Button>   
+        </Button>  
     </View>
   )
 }
@@ -78,5 +88,8 @@ const styles = StyleSheet.create({
     charContainer:{
         margin: 20,
         justifyContent:'center',
+    },
+    fonts:{
+        marginBottom: 8,
     }
 })
